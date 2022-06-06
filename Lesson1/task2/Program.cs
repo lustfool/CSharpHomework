3650c145424ab591a5b5f7404788688dc1b11f82
Mihail <holymolly5@yandex.ru>
2022-06-07 00:19:43 +0300

    solved second task

---
+++ Lesson1/task2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите первое число => ");
+string paramStringFirst = Console.ReadLine();
+int paramIntFirst = int.Parse(paramStringFirst);
+Console.Write("Введите второе число => ");
+string paramStringSecond = Console.ReadLine();
+int paramIntSecond = int.Parse(paramStringSecond);
+Console.Write("Введите третье число => ");
+string paramStringThird = Console.ReadLine();
+int paramIntThird = int.Parse(paramStringThird);
+
+int max = paramIntFirst;
+if (paramIntSecond >= max && paramIntSecond >= paramIntThird) //больше или равно указано для того случая, когда среди введённых есть два одинаковых числа
+{
+    max = paramIntSecond; 
+}
+if (paramIntThird >= max && paramIntThird >= paramIntSecond)
+{
+    max = paramIntThird;
+}
+Console.Write($"Максимальное из этих чисел {max}");
