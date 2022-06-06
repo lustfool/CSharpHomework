1c3733fdf1335b14663434378594407a4c07f75e
Mihail <holymolly5@yandex.ru>
2022-06-06 23:51:51 +0300

    solved first task

---
+++ Lesson1/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число => ");
+string paramStringFirst = Console.ReadLine();
+int paramIntFirst = int.Parse(paramStringFirst);
+Console.Write("Введите второе число => ");
+string paramStringSecond = Console.ReadLine();
+int paramIntSecond = int.Parse(paramStringSecond);
+
+if (paramIntFirst > paramIntSecond)
+{
+    Console.Write($"Большее число =  {paramIntFirst}, меньшее число = {paramIntSecond}");
+}
+else
+{
+    Console.Write($"Большее число =  {paramIntSecond}, меньшее число = {paramIntFirst}");
+}
