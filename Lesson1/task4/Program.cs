3ccc3c7d1333d34ed32ffdbf64d3f7596fe43379
Mihail <holymolly5@yandex.ru>
2022-06-07 18:56:31 +0300

    changed third a bit and solver fourth task

---
+++ Lesson1/task4/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число => ");
+string paramString = Console.ReadLine();
+int paramInt = int.Parse(paramString);
+
+int n = 1;
+Console.Write("Чётные числа: ");
+while (n <= paramInt)
+{
+    if (n % 2 == 0)
+    {
+        Console.Write($"{n}, ");
+    }
+    n = n + 1;
+}
