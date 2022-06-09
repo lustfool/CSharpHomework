b8ed23afb918ee06b2d8d05b1c68ec9e3d3057d5
Mihail <holymolly5@yandex.ru>
2022-06-09 23:49:11 +0300

    solved 3th task from 2nd lesson, solution is not optimal

---
+++ Lesson2/task3/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 3: Напишите программу, которая выводит третью
+// цифру заданного числа или сообщает, что третьей цифры
+// нет.
+
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+if (number.Length < 3) Console.WriteLine("Третьей цифры нет");
+else Console.WriteLine("Третья цифра числа: " + number[2]);
