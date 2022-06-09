8494537b75d2b8363091e9d4ccb6a3859ff0f3e0
Mihail <holymolly5@yandex.ru>
2022-06-09 22:13:32 +0300

    solved 1st task from 2nd lesson

---
+++ Lesson2/task1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 1: Напишите программу, которая принимает на
+// вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа.
+
+Console.Write("Введите трёхзначное число: ");
+string paramString = Console.ReadLine();
+int paramInt = int.Parse(paramString);
+
+if (paramString.Length != 3) Console.WriteLine("Введено неверное число!");
+else Console.Write("Вторая цифра числа - " + (paramInt % 100)/10);
