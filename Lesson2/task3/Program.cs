62a586a63e7a1ea15722c55d764ca402278dd352
Mihail <holymolly5@yandex.ru>
2022-06-10 01:07:54 +0300

    changer 3rd task from lesson 2

--- Lesson2/task3/Program.cs
+++ Lesson2/task3/Program.cs
@@ -2,7 +2,27 @@
 // цифру заданного числа или сообщает, что третьей цифры
 // нет.
 
-Console.Write("Введите число: ");
-string number = Console.ReadLine();
-if (number.Length < 3) Console.WriteLine("Третьей цифры нет");
-else Console.WriteLine("Третья цифра числа: " + number[2]);
+// Console.Write("Введите число: ");
+// string number = Console.ReadLine();
+// if (number.Length < 3) Console.WriteLine("Третьей цифры нет");
+// else Console.WriteLine("Третья цифра числа: " + number[2]);
+
+int prompt(string message)
+{
+    Console.Write(message);
+    string number = Console.ReadLine();
+    int numberInt = int.Parse(number);
+    return numberInt;
+}
+int numberInt = prompt("Введите число ");
+if (numberInt < 100) Console.WriteLine("Третьей цифры нет");
+else
+{
+    while (numberInt > 999)
+    {
+        numberInt = numberInt / 10;
+    }
+    numberInt = numberInt % 100;
+    numberInt = numberInt % 10;
+    Console.WriteLine("Третья цифра числа: " + numberInt);
+}
