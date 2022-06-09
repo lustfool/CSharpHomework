987b79a519d8bb8bb7c7f70ee101d746465d25b6
Mihail <holymolly5@yandex.ru>
2022-06-09 22:13:51 +0300

    solved 2nd task from 2nd lesson

---
+++ Lesson2/task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2: Напишите программу, которая выводит
+// случайное трёхзначное число и удаляет вторую цифру
+// этого числа.
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine("Случайное трёхзначное число: " + number);
+
+int[] array = new int[3];
+
+array[0] = number / 100;
+array[1] = (number % 100) / 10;
+array[2] = number % 10;
+
+Console.WriteLine("Получившееся число без второй цифры: " + array[0]+array[2]);
