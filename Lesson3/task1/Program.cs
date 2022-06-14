18eb35459addf7e1956e174531b2ba4912ec4869
Mihail <holymolly5@yandex.ru>
2022-06-14 22:35:34 +0300

    solved 1st task and its enhanced version

---
+++ Lesson3/task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход пятизначное 
+// число и проверяет, является ли оно палиндромом.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string param = Console.ReadLine();
+    return int.Parse(param);
+}
+
+int number = Prompt("Введите пятизначное число: ");
+
+int i1 = number / 10000;
+int i2 = (number % 10000) / 1000;
+int i4 = (number % 100) / 10;
+int i5 = number % 10;
+
+
+if (i1 == i5 && i2 == i4) System.Console.WriteLine("Число являяется палиндромом");
+else System.Console.WriteLine("Число не является палиндромом");
---
+++ Lesson3/task1enhanced/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 1 усложнённая: Напишите программу, которая принимает на вход ЛЮБОЕ 
+// число и проверяет, является ли оно палиндромом.
+
+Console.Write("Введите число => ");
+string paramString = Console.ReadLine();
+int paramInt = int.Parse(paramString);
+
+int ln = paramString.Length - 1;
+int iFirst = 0;
+int iLast = 0;
+
+while (ln >= 1)
+{
+    iFirst = paramInt / Convert.ToInt32(Math.Pow(10, ln));
+    iLast = paramInt % 10;
+    if (iFirst == iLast)
+    {
+        paramInt = (paramInt % Convert.ToInt32(Math.Pow(10, ln))) / 10;
+        ln = ln - 2;
+    }
+    else
+    {
+        System.Console.WriteLine("Число не является палиндромом");
+        Environment.Exit(0);
+    }
+}
+System.Console.WriteLine("Число является палиндромом");
