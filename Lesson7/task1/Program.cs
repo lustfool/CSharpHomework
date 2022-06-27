e9a5765a18234180b720f0c664d35d5d29087f59
Mihail <holymolly5@yandex.ru>
2022-06-27 22:29:03 +0300

    solved 1st task of 7th HW

---
+++ Lesson7/task1/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задача 1: Задайте двумерный массив размером m×n, 
+//заполненный случайными вещественными числами.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+double[,] GenerateArray(int row, int column)
+{
+    var array = new double[row, column];
+    var rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.NextDouble() * 100;
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]:F3}" + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int row = Prompt("Введите количество строк => ");
+int column = Prompt("Введите количество столбцов => ");
+double[,] myArray = GenerateArray(row, column);
+PrintArray(myArray);
