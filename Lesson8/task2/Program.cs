879513a3ee706451900e4b847cb498cb41d2ede4
Mihail <holymolly5@yandex.ru>
2022-07-01 19:13:09 +0300

    solved 2nd and 3rd task of the 8th HW

---
+++ Lesson8/task2/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 2: Задайте прямоугольный двумерный массив. Напишите программу, 
+//которая будет находить строку с наименьшей суммой элементов.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int[,] GenerateArray(int row, int column, int min, int max)
+{
+    var array = new int[row, column];
+    var rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int FindLowestSummInARow(int[,] array)
+{
+    int[] sumInRowArray = new int[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sumInRowArray[i] = sumInRowArray[i] + array[i, j];
+        }
+    }
+    int minimumSum = sumInRowArray[0];
+    int minimumRow = 1;
+    for (int k = 1; k < sumInRowArray.Length; k++)
+        if (sumInRowArray[k] < minimumSum)
+        {
+            minimumSum = sumInRowArray[k];
+            minimumRow = k + 1;
+        }
+    return minimumRow;
+}
+
+int row = Prompt("Введите количество строк => ");
+int column = Prompt("Введите количество столбцов => ");
+int min = 0;
+int max = 10;
+int[,] myArray = GenerateArray(row, column, min, max);
+Print2DArray(myArray);
+System.Console.WriteLine($"Строка с наименьшей суммой элементов => {FindLowestSummInARow(myArray)}");
---
+++ Lesson8/task3/Program.cs
@@ -0,0 +1,76 @@
+﻿//Задача 3: Задайте две матрицы. Напишите программу, 
+//которая будет находить произведение двух матриц.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int[,] GenerateArray(int row, int column, int min, int max)
+{
+    var array = new int[row, column];
+    var rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void CompositionPossibility (int columnFirst, int rawSecond)
+{
+    if (columnFirst != rawSecond)
+    {
+        Console.WriteLine("Такие матрицы перемножать нельзя! Число столбцов 1й матрицы должно быть равно числу строк второй.");
+        Environment.Exit(0);
+    }
+}
+
+int [,] CompositionOfTwoMatrix (int[,] firstMatrix, int[,] secondMatrix)
+{
+    int [,] compositionMatrix = new int [firstMatrix.GetLength(0), secondMatrix.GetLength(1)];
+    for (int i = 0; i < compositionMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < compositionMatrix.GetLength(1); j++)
+        {
+            for (int k = 0; k <= compositionMatrix.GetLength(1); k++)
+            {
+                compositionMatrix [i,j] = compositionMatrix [i,j] + firstMatrix [i,k]*secondMatrix[k,j];
+            }
+        }
+    }
+    return compositionMatrix;
+}
+
+int rowFirst = Prompt("Введите количество строк 1й матрицы => ");
+int columnFirst = Prompt("Введите количество столбцов 1й матрицы => ");
+int rowSecond = Prompt("Введите количество сторок 2й матрицы => ");
+int columnSecond = Prompt("Введите количество столбцов 2й матрицы => ");
+int min = 0;
+int max = 10;
+CompositionPossibility(columnFirst, rowSecond);
+int[,] myFirstMatrix = GenerateArray(rowFirst, columnFirst, min, max);
+int[,] mySecondMatrix = GenerateArray(rowSecond, columnSecond, min, max);
+Console.WriteLine("1я матрица: ");
+Print2DArray(myFirstMatrix);
+Console.WriteLine("2я матрица: ");
+Print2DArray(mySecondMatrix);
+int [,] compositionMatrix = CompositionOfTwoMatrix (myFirstMatrix, mySecondMatrix);
+Console.WriteLine("Произведение двух матриц: ");
+Print2DArray(compositionMatrix);
