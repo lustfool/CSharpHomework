e500fb23121ac2a9eb1302d17dce148dbd93c2b6
Mihail <holymolly5@yandex.ru>
2022-07-04 22:22:30 +0300

    solved 1st task of the 9th HW

---
+++ Lesson9/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, 
+// которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+void PrintOddNaturalNumbers(int firstNumber, int secondNumber)
+{
+    if (firstNumber > secondNumber)
+    {
+        return;
+    }
+    if (firstNumber % 2 == 0)
+        Console.Write($"{firstNumber}; ");
+    PrintOddNaturalNumbers(firstNumber + 1, secondNumber);
+}
+
+int M = Prompt($"Введите 1е число => ");
+int N = Prompt($"Введите 2е число => ");
+Console.WriteLine($"Чётные натуральные числа в промежутке от {M} до {N}:");
+PrintOddNaturalNumbers(M, N);
