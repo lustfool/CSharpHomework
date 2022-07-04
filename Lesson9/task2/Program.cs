fef4de52e8d7d7c71db5ebee8d3653c4d80f640d
Mihail <holymolly5@yandex.ru>
2022-07-04 22:38:21 +0300

    solved 2nd task of the 9th HW

---
+++ Lesson9/task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int PrintEvenNaturalNumbers(int firstNumber, int secondNumber)
+{
+    if (firstNumber > secondNumber)
+    {
+        return 0;
+    }
+    return PrintEvenNaturalNumbers(firstNumber + 1, secondNumber) + firstNumber;
+}
+
+int M = Prompt($"Введите 1е число => ");
+int N = Prompt($"Введите 2е число => ");
+Console.WriteLine($"Сумма натуральных элементов в промежутке от {M} до {N}: {PrintEvenNaturalNumbers(M, N)}");
