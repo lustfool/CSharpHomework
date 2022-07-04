fa4e38a32221cee74155ac3c0d1e1ee5c88c7de5
Mihail <holymolly5@yandex.ru>
2022-07-04 22:39:52 +0300

    changed 2nd task of the 9th HW

--- Lesson9/task2/Program.cs
+++ Lesson9/task2/Program.cs
@@ -7,15 +7,15 @@ int Prompt(string message)
     return int.Parse(Console.ReadLine());
 }
 
-int PrintEvenNaturalNumbers(int firstNumber, int secondNumber)
+int SumOfNumbersInbetween(int firstNumber, int secondNumber)
 {
     if (firstNumber > secondNumber)
     {
         return 0;
     }
-    return PrintEvenNaturalNumbers(firstNumber + 1, secondNumber) + firstNumber;
+    return SumOfNumbersInbetween(firstNumber + 1, secondNumber) + firstNumber;
 }
 
 int M = Prompt($"Введите 1е число => ");
 int N = Prompt($"Введите 2е число => ");
-Console.WriteLine($"Сумма натуральных элементов в промежутке от {M} до {N}: {PrintEvenNaturalNumbers(M, N)}");
+Console.WriteLine($"Сумма натуральных элементов в промежутке от {M} до {N}: {SumOfNumbersInbetween(M, N)}");
