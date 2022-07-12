be11b7eccba5639b3d09704c3b208291d7783472
Mihail <holymolly5@yandex.ru>
2022-07-12 15:23:05 +0300

    tried to solve 10th HW

---
+++ Lesson10/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// // Задача 1: Есть монотонная последовательность, 
+// каждое число встречается ровно то количество раз, 
+// какое значение этого числа: 1, 2, 2, 3, 3, 3, 4, 4, 4, 4,... 
+// Выведите ряд этой 
+// последовательности до N-го значения, желательно использовать рекурсию:
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+
+void PrintTriangleSequence(int n, int k = 1)
+{
+    int count = (k + 1) * k / 2;
+    for (int i = count - k; i < count && i < n; i++)
+        Console.Write($"{k}, ");
+    if (count < n) PrintTriangleSequence(n, k + 1);
+}
+
+
+int n = Prompt("Введите количество элементов последовательности => ");
+PrintTriangleSequence(n);
---
+++ Lesson10/task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 2: Напишите программу вычисления функции 
+// Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n. 
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m < 0 || n < 0)
+    {
+        Console.WriteLine("Функция Аккермана принимает на вход только неотрицательные числа");
+        Environment.Exit(0);
+    }
+    if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+    else return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+int m = Prompt("Введите 1й параметр => ");
+int n = Prompt("Введите 2й параметр => ");
+Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
